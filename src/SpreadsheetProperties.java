dfb47bfe799495d62d3f15603fdd48edec2a3c46
Remco van Veenendaal <remco.van.veenendaal@nationaalarchief.nl>
2022-12-10 15:18:12 +0100

    December 2022 version
    Updated version of Apache POI-HSSF and POI-XSSF, and other libraries.
    More properties extracted by SCA.
    Code clean-up

--- src/SpreadsheetProperties.java
+++ src/SpreadsheetProperties.java
@@ -1,165 +1,26 @@
+import java.util.ArrayList;
+
 public class SpreadsheetProperties {
+	private FileProperties fileProperties = new FileProperties(); 					// A spreadsheet has one set of file properties
+	private WorkbookProperties workbookProperties = new WorkbookProperties();			// A spreadsheet has one set of workbook properties
+	private ArrayList<WorksheetProperties> worksheetPropertiesList = new ArrayList<>();		// A spreadsheet (workbook) can have multiple work sheets
 	
-	private int iFormulas = 0; // formulas present in spreadsheet
-	private int iHyperlinks = 0; // hyperlinks present in spreadsheet
-	private int iDefinedNames = 0; // defined names present in spreadsheet
-	private int iComments = 0; // comments present in spreadsheet
-	private int iWorkSheets = 0; // worksheets present in spreadsheet
-	private int iHasVBAMacros = 0; // spreadsheet has (had) vba macros: 0 = false, 1 or greater = true
-	private int iShapes= 0; // shapes present in spreadsheet
-	private int iDates = 0; // dates present in spreadsheet
-	private int iCellsUsed = 0; // cells used in spreadsheet
-	private int iFonts = 0; // fonts used in spreadsheet
-	private int iCellStyles = 0; // cell styles used in spreadsheet
-	private int iColors = 0; // colours used in spreadsheet
-	private int iExternalLinks = 0; // external links present in formulas (!) in spreadsheet
-	private double iFileSizeKb = 0; // size of file (in kilobyte)
-	private String sLastModified = ""; // last modified time (if available) 
-	private String sLastAccessed = ""; // last accessed time (if available)
-	private String sCreation = ""; // creation time (if available)
-	private int iHasRevisionHistory = 0; // 1 = spreadsheet has revision history on, 0 = off
-
-	public int getFormulas() {
-		return iFormulas;
+	public ArrayList<WorksheetProperties> getWorksheetPropertiesList() {
+		return worksheetPropertiesList;
 	}
-
-	public void setFormulas(int iFormulas) {
-		this.iFormulas = iFormulas;
+	public void setWorksheetPropertiesList(ArrayList<WorksheetProperties> worksheetPropertiesList) {
+		this.worksheetPropertiesList = worksheetPropertiesList;
 	}
-
-	public int getHyperlinks() {
-		return iHyperlinks;
+	public FileProperties getFileProperties() {
+		return fileProperties;
 	}
-
-	public void setHyperlinks(int iHyperlinks) {
-		this.iHyperlinks = iHyperlinks;
+	public void setFileProperties(FileProperties fileProperties) {
+		this.fileProperties = fileProperties;
 	}
-
-	public int getDefinedNames() {
-		return iDefinedNames;
+	public WorkbookProperties getWorkbookProperties() {
+		return workbookProperties;
 	}
-
-	public void setDefinedNames(int iDefinedNames) {
-		this.iDefinedNames = iDefinedNames;
-	}
-
-	public int getComments() {
-		return iComments;
-	}
-
-	public void setComments(int iComments) {
-		this.iComments = iComments;
-	}
-
-	public int getWorkSheets() {
-		return iWorkSheets;
-	}
-
-	public void setWorkSheets(int iWorkSheets) {
-		this.iWorkSheets = iWorkSheets;
-	}
-
-	public int getHasVBAMacros() {
-		return iHasVBAMacros;
-	}
-
-	public void setHasVBAMacros(int iHasVBAMacros) {
-		this.iHasVBAMacros = iHasVBAMacros;
-	}
-
-	public int getShapes() {
-		return iShapes;
-	}
-
-	public void setShapes(int iShapes) {
-		this.iShapes = iShapes;
-	}
-
-	public int getDates() {
-		return iDates;
-	}
-
-	public void setDates(int iDates) {
-		this.iDates = iDates;
-	}
-
-	public int getCellsUsed() {
-		return iCellsUsed;
-	}
-
-	public void setCellsUsed(int iCellsUsed) {
-		this.iCellsUsed = iCellsUsed;
-	}
-
-	public int getFonts() {
-		return iFonts;
-	}
-
-	public void setFonts(int iFonts) {
-		this.iFonts = iFonts;
-	}
-
-	public int getColors() {
-		return iColors;
-	}
-
-	public void setColors(int iColors) {
-		this.iColors = iColors;
-	}
-
-	public int getCellStyles() {
-		return iCellStyles;
-	}
-
-	public void setCellStyles(int iCellStyles) {
-		this.iCellStyles = iCellStyles;
-	}
-
-	public int getExternalLinks() {
-		return iExternalLinks;
-	}
-
-	public void setExternalLinks(int iExternalLinks) {
-		this.iExternalLinks = iExternalLinks;
-	}
-
-	public double getFileSizeKb() {
-		return iFileSizeKb;
-	}
-
-	public void setFileSizeKb(double iFileSizeKb) {
-		this.iFileSizeKb = iFileSizeKb;
-	}
-
-	public String getLastModified() {
-		return sLastModified;
-	}
-
-	public void setLastModified(String sLastModified) {
-		this.sLastModified = sLastModified;
-	}
-
-	public String getLastAccessed() {
-		return sLastAccessed;
-	}
-
-	public void setLastAccessed(String sLastAccessed) {
-		this.sLastAccessed = sLastAccessed;
-	}
-
-	public String getCreation() {
-		return sCreation;
-	}
-
-	public void setCreation(String sCreation) {
-		this.sCreation = sCreation;
-	}
-
-	public int getHasRevisionHistory() {
-		return iHasRevisionHistory;
-	}
-
-	public void setHasRevisionHistory(int iHasRevisionHistory) {
-		this.iHasRevisionHistory = iHasRevisionHistory;
+	public void setWorkbookProperties(WorkbookProperties workbookProperties) {
+		this.workbookProperties = workbookProperties;
 	}
 }
---
+++ src/WorksheetProperties.java
@@ -0,0 +1,94 @@
+public class WorksheetProperties {
+	public String sSheetName = ""; 
+	public int iPivotTables = 0;
+	public int iTables = 0;
+	public int iShapes = 0;
+	public int iCharts = 0;
+	public int iRowsUsed = 0;
+	public int iPhysicallyUsedRows = 0;
+	public int iCellsUsed = 0;
+	public int iPhysicallyUsedCells = 0;
+	public int iFormulas = 0;
+	public int iDates = 0;
+	public int iHyperlinks = 0;
+	public int iCellComments = 0;
+
+	public String getsSheetName() {
+		return sSheetName;
+	}
+	public void setsSheetName(String sSheetName) {
+		this.sSheetName = sSheetName;
+	}
+	public int getiPivotTables() {
+		return iPivotTables;
+	}
+	public void setiPivotTables(int iPivotTables) {
+		this.iPivotTables = iPivotTables;
+	}
+	public int getiTables() {
+		return iTables;
+	}
+	public void setiTables(int iTables) {
+		this.iTables = iTables;
+	}
+	public int getiShapes() {
+		return iShapes;
+	}
+	public void setiShapes(int iShapes) {
+		this.iShapes = iShapes;
+	}
+	public int getiCharts() {
+		return iCharts;
+	}
+	public void setiCharts(int iCharts) {
+		this.iCharts = iCharts;
+	}
+	public int getiRowsUsed() {
+		return iRowsUsed;
+	}
+	public void setiRowsUsed(int iRowsUsed) {
+		this.iRowsUsed = iRowsUsed;
+	}
+	public int getiPhysicallyUsedRows() {
+		return iPhysicallyUsedRows;
+	}
+	public void setiPhysicallyUsedRows(int iPhysicallyUsedRows) {
+		this.iPhysicallyUsedRows = iPhysicallyUsedRows;
+	}
+	public int getiCellsUsed() {
+		return iCellsUsed;
+	}
+	public void setiCellsUsed(int iCellsUsed) {
+		this.iCellsUsed = iCellsUsed;
+	}
+	public int getiPhysicallyUsedCells() {
+		return iPhysicallyUsedCells;
+	}
+	public void setiPhysicallyUsedCells(int iPhysicallyUsedCells) {
+		this.iPhysicallyUsedCells = iPhysicallyUsedCells;
+	}
+	public int getiFormulas() {
+		return iFormulas;
+	}
+	public void setiFormulas(int iFormulas) {
+		this.iFormulas = iFormulas;
+	}
+	public int getiDates() {
+		return iDates;
+	}
+	public void setiDates(int iDates) {
+		this.iDates = iDates;
+	}
+	public int getiHyperlinks() {
+		return iHyperlinks;
+	}
+	public void setiHyperlinks(int iHyperlinks) {
+		this.iHyperlinks = iHyperlinks;
+	}
+	public int getiCellComments() {
+		return iCellComments;
+	}
+	public void setiCellComments(int iCellComments) {
+		this.iCellComments = iCellComments;
+	}	
+}
